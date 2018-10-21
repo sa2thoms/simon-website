d7174c02712a6ddaf9ef91fa50c43911c0b5a82c
Simon Thomson <sa2thoms@edu.uwaterloo.ca>
2018-10-20 21:46:59 -0400

    Added proper routing to project

--- src/app/boss-battle/boss-battle-component/boss-battle.component.ts
+++ src/app/boss-battle/boss-battle-component/boss-battle.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-boss-battle',
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BossBattleComponent implements OnInit {
 
+  playArea = {
+    width: 640,
+    height: 480,
+  };
+
+  @ViewChild('gameCanvas') gameCanvas: ElementRef;
+  gl: WebGLRenderingContext;
+
   constructor() { }
 
   ngOnInit() {
+    this.gl = this.gameCanvas.nativeElement.getContext('webgl');
+    if (!this.gl) {
+      throw new Error('Web GL not supported or not working');
+    }
+
+    // Set clear color to black, fully opaque
+    this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    // Clear the color buffer with specified clear color
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT);
   }
 
 }
